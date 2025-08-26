3af59b9741d3a7e212b42b82ce3e3b24c75cc3c7
Gellert <whitemagic295@gmail.com>
2025-08-26 21:09:55 +0300

    opening hours settings frontend finished

--- resources/js/react/admin/Settings.jsx
+++ resources/js/react/admin/Settings.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
-import TimePicker from 'react-bootstrap-time-picker';
 import OpeningHours from './Settings/OpeningHours';
 
 function Settings () {
 
-    const [time, setTime] = useState('10');
     const [showingOpeningHours, setShowingOpeningHours] = useState(false);
 
 
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row mb-3">
                 <label htmlFor="sessionLength" className='col-form-label col-6'>Session length</label>
                 <div className="col-6">
@@ -23,21 +21,11 @@ function Settings () {
             <div className="row mb-3">
                 <h5>Opening hours</h5>
 
-                <button onClick={() => setShowingOpeningHours(true)} data-bs-toggle="modal" data-bs-target="#editOpeningHours">Employee ➔</button>
+                <button className="btn btn-primary" onClick={() => setShowingOpeningHours(true)} data-bs-toggle="modal" data-bs-target="#editOpeningHours">Employee ➔</button>
 
                 {showingOpeningHours && (
                     <OpeningHours/>
                 )}
-
-               <TimePicker
-                    format={24}
-                    start="10"
-                    end="23"
-                    step={15}
-                    value={time}
-                    onChange={setTime}
-                />
-                <p className="mt-2">Selected time: {time}</p>
             </div>
         </div>
     )
--- resources/js/react/admin/Settings/OpeningHours.jsx
+++ resources/js/react/admin/Settings/OpeningHours.jsx
@@ -3,14 +3,16 @@ import TimePicker from 'react-bootstrap-time-picker';
 
 function OpeningHours () {
     const [openingHours, setOpeningHours] = useState({
-        monday: {open: "08:00", close: "18:00"},
-        tuesday: {open: "08:00", close: "18:00"},
-        wednesday: {open: "08:00", close: "18:00"},
-        thursday: {open: "08:00", close: "18:00"},
-        friday: {open: "08:00", close: "18:00"},
+        monday: {open: "08:00", close: "16:00", closed: false},
+        tuesday: {open: "08:00", close: "16:00", closed: false},
+        wednesday: {open: "08:00", close: "16:00", closed: false},
+        thursday: {open: "08:00", close: "16:00", closed: false},
+        friday: {open: "08:00", close: "16:00", closed: false},
+        saturday: {open: "08:00", close: "16:00", closed: true},
+        sunday: {open: "08:00", close: "16:00", closed: true},
     })
 
-    const days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+    const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
     return (
         <div className="modal fade" id="editOpeningHours" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -23,8 +25,8 @@ function OpeningHours () {
                     <div className="modal-body">
                         {days.map(day => (
                             <div key={day} className="row mb-3">
-                                <label htmlFor={day} className='col-form-label col-sm-6 text-capitalize'>{day}</label>
-                                <div className="col-sm-6">
+                                <label htmlFor={day} className='col-form-label col-sm-5 text-capitalize'>{day}</label>
+                                <div className="col-sm-7">
                                     <div className="input-group">
                                         <TimePicker
                                             format={24}
@@ -32,6 +34,7 @@ function OpeningHours () {
                                             end="23"
                                             step={30}
                                             value={openingHours[day].open}
+                                            disabled={openingHours[day].closed}
                                             onChange={(value) => setOpeningHours(prev => ({
                                                 ...prev, [day]: {...prev[day], open: value}
                                             }))}
@@ -43,14 +46,33 @@ function OpeningHours () {
                                             end="23"
                                             step={30}
                                             value={openingHours[day].close}
+                                            disabled={openingHours[day].closed}
                                             onChange={(value) => setOpeningHours(prev => ({
                                                 ...prev, [day]: {...prev[day], close: value}
                                             }))}
                                         />
+                                        <div className="input-group-text">
+                                            <input
+                                                className="form-check-input"
+                                                type="checkbox"
+                                                checked={openingHours[day].closed}
+                                                onChange={(e) =>
+                                                    setOpeningHours(prev => ({
+                                                    ...prev,
+                                                    [day]: { ...prev[day], closed: e.target.checked }
+                                                    }))
+                                                }
+                                            />
+                                            <label className="ms-2">Closed</label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         ))}
+
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-primary">Save</button>
                     </div>
                 </div>
             </div>
